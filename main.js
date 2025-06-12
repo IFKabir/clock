acdc9c10fd4876938dc1aeebedab94b72fc70677
Royal Srivastava <157229547+royalsrivastavagithub@users.noreply.github.com>
2025-06-13 03:20:59 +0530

    tape fix : phantom title bar (ghost)

--- main.js
+++ main.js
@@ -5,24 +5,39 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 200,
         height: 100,
-        minHeight: 100,
-        maxHeight: 100,
-        minWidth: 200,
-        maxWidth: 200,
+   resizable:false,
         maximizable: false,
         frame: false,
         fullscreen: false,     // no window frame (optional)
         transparent: true,
-        alwaysOnTop: false,    // set true if you want it above other windows
+        alwaysOnTop: true,    // set true if you want it above other windows
         webPreferences: {
             nodeIntegration: true,   // allow require() in renderer (not needed here but set if needed)
             contextIsolation: false
         }
     });
+    //fixes phantombar on windows 
+    //check https://github.com/electron/electron/issues/39959
     win.loadFile('index.html');
+               win.on("blur", () => {
+		win.setResizable(true);
+		const [w, h] = win.getSize();
+		win.setSize(w, h + 1);
+		win.setSize(w, h);
+		win.setResizable(false);
+	});
+	win.on("focus", () => {
+		win.setResizable(true);
+		const [w, h] = win.getSize();
+		win.setSize(w, h + 1);
+		win.setSize(w, h);
+		win.setResizable(false);
+	});
 }
 
 app.whenReady().then(createWindow);
+
+
 
 // Quit when all windows are closed (except on macOS)
 app.on('window-all-closed', () => {
